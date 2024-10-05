75cf4545d51c59307a51ffe278df2e1b519fba1d
M-KC <macbookpro@192.168.2.20>
2024-10-05 17:08:49 +0700

    upenv

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,52 +1,68 @@
-const jwt = require('jsonwebtoken')
-const dotenv = require('dotenv')
-dotenv.config()
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const authMiddleWare = (req, res, next) =>{
-    const token = req.headers.token?.split(' ')[1]
-    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, user) {
-        if(err){
-            return res.status(404).json({
-                message: 'The authemtication',
+const authMiddleWare = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({
+            message: 'Token not provided',
+            status: 'ERROR'
+        });
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+        if (err) {
+            return res.status(401).json({
+                message: 'Invalid or expired token',
                 status: 'ERROR'
-            })
+            });
         }
-        if(user.isAdmin){
 
-            next()
-        }else{
-            return res.status(404).json({
-                message: 'The authemtication',
+        if (user.isAdmin) {
+            next();
+        } else {
+            return res.status(403).json({
+                message: 'Access denied',
                 status: 'ERROR'
-            })
-        }
-    });
-}
-
-const authUserMiddleWare = (req, res, next) =>{
-    const token = req.headers.token?.split(' ')[1]
-    const userId = req.params.id
-    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, user) {
-        if(err){
-            return res.status(404).json({
-                message: 'The authemtication',
-                status: 'ERROR'
-            })
-        }
-        console.log('user', user)
-        if(user.isAdmin || user.id === userId){
-
-            next()
-        }else{
-            return res.status(404).json({
-                message: 'The authemtication',
-                status: 'ERROR'
-            })
+            });
         }
     });
-}
+};
 
-module.exports={
+const authUserMiddleWare = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    const userId = req.params.id;
+
+    if (!token) {
+        return res.status(401).json({
+            message: 'Token not provided',
+            status: 'ERROR'
+        });
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+        if (err) {
+            return res.status(401).json({
+                message: 'Invalid or expired token',
+                status: 'ERROR'
+            });
+        }
+
+        console.log('user', user);
+        if (user.isAdmin || user.id === userId) {
+            next();
+        } else {
+            return res.status(403).json({
+                message: 'Access denied',
+                status: 'ERROR'
+            });
+        }
+    });
+};
+
+module.exports = {
     authMiddleWare,
     authUserMiddleWare
-}
+};
