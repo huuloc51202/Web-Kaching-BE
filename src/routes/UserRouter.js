6040747cb09aa2445ad6b5145a59e273f5a14049
M-KC <macbookpro@192.168.2.20>
2024-10-06 17:59:12 +0700

    upenv

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const UserController = require('../controllers/UserController')
-const { authMiddleWare, authUserMiddleWare } = require('../middleware/authMiddleware')
+const { authMiddleWare, authUserMiddleWare } = require('middleware/authMiddleware')
 
 router.post('/sign-up' , UserController.createUser)
 router.post('/sign-in' , UserController.loginUser)
