6bcd91f374cfa4528ad0fd3914b9871b9f28e15a
M-KC <macbookpro@192.168.2.20>
2024-10-06 22:10:31 +0700

    uppj

--- src/routes/OrderRouter.js
+++ src/routes/OrderRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const OrderController = require('../controllers/OrderController')
-const { authMiddleWare, authUserMiddleWare } = require('../middleware/authMiddleWare');
+const {authUserMiddleWare, authMiddleWare } = require('../middleware/authMiddleware')
 
 router.post('/create' , OrderController.createOrder)
 router.get('/get-all-order/:id',authUserMiddleWare, OrderController.getAllOrderDetails)
--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const ProductController = require('../controllers/ProductController')
-const { authMiddleWare, authUserMiddleWare } = require('../middleware/authMiddleWare');
-
+const { authMiddleWare } = require('../middleware/authMiddleware')
 
 router.post('/create' , ProductController.createProduct)
 router.put('/update-product/:id',authMiddleWare , ProductController.updateProduct)
